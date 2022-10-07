92825e470f405417191ceb45865d03ab5686186b
alpari <alpari@ttu.ee>
2022-10-07 15:58:10 +0300

    User GetMapping method test

--- src/main/java/ee/taltech/iti0302/user/UserController.java
+++ src/main/java/ee/taltech/iti0302/user/UserController.java
@@ -1,7 +1,10 @@
 package ee.taltech.iti0302.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -11,5 +14,10 @@ public class UserController {
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping("/api")
+    public List<User> getUsers() {
+        return userService.getUsers();
     }
 }
