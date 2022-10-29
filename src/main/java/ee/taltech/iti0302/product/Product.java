075f200551bd9802aad9de83ff3f2d926154c975
illast <illast@ttu.ee>
2022-10-29 17:25:47 +0300

    Added product table to postgres

--- src/main/java/ee/taltech/iti0302/product/Product.java
+++ src/main/java/ee/taltech/iti0302/product/Product.java
@@ -1,68 +1,31 @@
 package ee.taltech.iti0302.product;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter @Setter
+@Entity
+@Table(name = "product")
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-    private Integer userId;
     private String name;
-    private Integer type;
     private String description;
-    private String status;
 
-    public Product(Integer id, Integer userId, String name, Integer type, String description, String status) {
+    public Product(Integer id, String name, String description) {
         this.id = id;
-        this.userId = userId;
         this.name = name;
-        this.type = type;
-        this.description = description;
-        this.status = status;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getType() {
-        return type;
-    }
-
-    public void setType(Integer type) {
-        this.type = type;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
         this.description = description;
     }
 
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
+    public Product() {
     }
 }
