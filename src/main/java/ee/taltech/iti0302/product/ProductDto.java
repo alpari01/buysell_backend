7877b2e3574b99e999eb4976eed785712598226d
illast <illast@ttu.ee>
2022-10-29 20:58:24 +0300

    @Getter @Setter @Data work

--- src/main/java/ee/taltech/iti0302/product/ProductDto.java
+++ src/main/java/ee/taltech/iti0302/product/ProductDto.java
@@ -1,32 +1,13 @@
 package ee.taltech.iti0302.product;
 
+import lombok.Data;
+
+@Data
 public class ProductDto {
 
     private Integer id;
     private String name;
     private String description;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
+    private String userFirstName;
+    private String userLastName;
 }
