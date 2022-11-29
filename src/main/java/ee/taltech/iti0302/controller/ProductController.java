3c395fe2ca0579e73e027a8aab0ac5f62a1ebf99
illast <illast@ttu.ee>
2022-11-29 17:07:08 +0200

    Added deleteMapping for products

--- src/main/java/ee/taltech/iti0302/controller/ProductController.java
+++ src/main/java/ee/taltech/iti0302/controller/ProductController.java
@@ -5,7 +5,9 @@ import ee.taltech.iti0302.repository.ProductFilter;
 import ee.taltech.iti0302.repository.ProductResponse;
 import ee.taltech.iti0302.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +28,11 @@ public class ProductController {
     @PostMapping("/api/products")
     public void addProduct(@RequestBody ProductDto productDto) {
         productService.addProduct(productDto);
+    }
+
+    @DeleteMapping("/api/products/{productId}")
+    public void deleteProduct(@PathVariable("productId") Integer id) {
+        productService.deleteProductById(id);
     }
 
     @GetMapping("/api/products2")
