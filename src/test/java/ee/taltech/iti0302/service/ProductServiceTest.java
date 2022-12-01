6b0f126cdb152087f80f0201c022f415622fb42c
illast <illast@ttu.ee>
2022-12-01 19:27:52 +0200

    Added ProductServiceTest class Added getUsers and getProducts tests

---
+++ src/test/java/ee/taltech/iti0302/service/ProductServiceTest.java
@@ -0,0 +1,56 @@
+package ee.taltech.iti0302.service;
+
+import ee.taltech.iti0302.dto.ProductDto;
+import ee.taltech.iti0302.mapper.ProductMapper;
+import ee.taltech.iti0302.mapper.ProductMapperImpl;
+import ee.taltech.iti0302.model.Product;
+import ee.taltech.iti0302.repository.ProductRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceTest {
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @Spy
+    private ProductMapper productMapper = new ProductMapperImpl();
+
+    @InjectMocks
+    private ProductService productService;
+
+    @Test
+    void getProducts() {
+        Product product1 = Product.builder().id(1).name("Milk").description("...").build();
+        Product product2 = Product.builder().id(2).name("Milk").description("...").build();
+        Product product3 = Product.builder().id(3).name("Milk").description("...").build();
+        Product product4 = Product.builder().id(4).name("Milk").description("...").build();
+        List<Product> products = new ArrayList<>(List.of(product1, product2, product3, product4));
+        given(productRepository.findAll()).willReturn(products);
+
+        // when
+        var result = productService.getProducts();
+
+        // then
+        then(productMapper).should().toDtoList(products);
+        then(productRepository).should().findAll();
+        ProductDto productDto1 = ProductDto.builder().id(1).name("Milk").description("...").build();
+        ProductDto productDto2 = ProductDto.builder().id(2).name("Milk").description("...").build();
+        ProductDto productDto3 = ProductDto.builder().id(3).name("Milk").description("...").build();
+        ProductDto productDto4 = ProductDto.builder().id(4).name("Milk").description("...").build();
+        List<ProductDto> productDtos = new ArrayList<>(List.of(productDto1, productDto2, productDto3, productDto4));
+        assertEquals(productDtos, result);
+    }
+}
--- src/test/java/ee/taltech/iti0302/service/UserServiceTest.java
+++ src/test/java/ee/taltech/iti0302/service/UserServiceTest.java
@@ -12,6 +12,8 @@ import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -29,6 +31,30 @@ class UserServiceTest {
 
     @InjectMocks
     private UserService userService;
+
+    @Test
+    void getUsers() {
+        // given
+        User user1 = User.builder().id(1).firstName("Ilja").lastName("Lastovko").build();
+        User user2 = User.builder().id(2).firstName("Alan").lastName("Parik").build();
+        User user3 = User.builder().id(3).firstName("Mart").lastName("Hütt").build();
+        User user4 = User.builder().id(4).firstName("Siim").lastName("Rebane").build();
+        List<User> users = new ArrayList<>(List.of(user1, user2, user3, user4));
+        given(userRepository.findAll()).willReturn(users);
+
+        // when
+        var result = userService.getUsers();
+
+        // then
+        then(userMapper).should().toDtoList(users);
+        then(userRepository).should().findAll();
+        UserDto userDto1 = UserDto.builder().id(1).firstName("Ilja").lastName("Lastovko").build();
+        UserDto userDto2 = UserDto.builder().id(2).firstName("Alan").lastName("Parik").build();
+        UserDto userDto3 = UserDto.builder().id(3).firstName("Mart").lastName("Hütt").build();
+        UserDto userDto4 = UserDto.builder().id(4).firstName("Siim").lastName("Rebane").build();
+        List<UserDto> userDtos = new ArrayList<>(List.of(userDto1, userDto2, userDto3, userDto4));
+        assertEquals(userDtos, result);
+    }
 
     @Test
     void getUserById() {
