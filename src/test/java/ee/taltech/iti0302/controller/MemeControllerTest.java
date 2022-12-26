d0d4ed4ac34068a4b05f6d720f55722573468b20
illast <illast@ttu.ee>
2022-12-26 19:26:50 +0200

    Added integration tests

---
+++ src/test/java/ee/taltech/iti0302/controller/MemeControllerTest.java
@@ -0,0 +1,29 @@
+package ee.taltech.iti0302.controller;
+
+import ee.taltech.iti0302.AbstractIntegrationTest;
+import liquibase.pro.packaged.S;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc
+class MemeControllerTest extends AbstractIntegrationTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void getMeme() throws Exception {
+        mvc.perform(get("/api/public/meme").with(user("user")))
+                .andExpect(status().isOk());
+    }
+}
